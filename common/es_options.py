175bf9a077d5c72e90158f6e7ac8f721dd6b89fc
Matthias De Bie <mattydebie@gmail.com>
2018-05-31 13:42:43 +0200

    Refs #37870: update backend logic to allow for easy filtering

--- common/es_options.py
+++ common/es_options.py
@@ -9,7 +9,10 @@ class EsOptions(object):
     def __init__(self):
         self.esOpts = {}
         self.matches = {}
-        self.filters = {}
+        self.filters = {
+            'include': {},
+            'exclude': {}
+        }
         self.setPaging()
 
         reverse = optsParser.parse_args()['reverse']
@@ -41,12 +44,17 @@ class EsOptions(object):
         if not value is None:
             self.matches[key] = value
 
-    def addFilter(self, key, value: str):
+    def addFilter(self, key: str, value: str):
         if not value is None:
-            type = 'must_not' if value.startswith('!') else 'filter'  # 'must'
-            self.__addKeyIfNotExists__(self.filters, type)
-            self.__addKeyIfNotExists__(self.filters[type], 'match')
-            self.filters[type]['match'][key] = value.replace('!', '')
+            if ',' in value:
+                for val in value.split(','):
+                    self.addFilter(key, val)
+            else:
+                incType = 'exclude' if value.startswith('!') else 'include'
+                if not key in self.filters[incType]:
+                    self.filters[incType][key] = []
+
+                self.filters[incType][key].append(value[1:] if value.startswith('!') else value)
 
     def get(self):
         if self.matches:
@@ -55,8 +63,22 @@ class EsOptions(object):
 
         if self.filters:
             self.__addKeyIfNotExists__(self.esOpts, 'query')
-            self.__addKeyIfNotExists__(self.esOpts['query'], 'bool')
-            self.esOpts['query']['bool'] = self.filters
+            self.esOpts['query']['bool'] = {'must': [], 'must_not': []}
+            self.esOpts['query']['bool']['must'].append({'bool': {'should': []}})
+
+            for key, value in self.filters['include'].items():
+                if len(value) == 1:
+                    self.esOpts['query']['bool']['must'].append({'term': {key: value[0]}})
+                else:
+                    for item in value:
+                        self.esOpts['query']['bool']['must'][0]['bool']['should'].append({'term': {key: item}})
+
+            for key, value in self.filters['exclude'].items():
+                if len(value) == 1:
+                    self.esOpts['query']['bool']['must_not'].append({'term': {key: value[0]}})
+                else:
+                    for item in value:
+                        self.esOpts['query']['bool']['must_not'].append({'term': {key: item}})
 
         return self.esOpts
 
